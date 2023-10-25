b13e0ae5de4a15e2d5492d74a21c5577c60c7814
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-10-25 15:19:41 -0400

    Add mobile nav basic style and click

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// mobile nav
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcn = document.querySelector('.mnav__close-btn-icon');
+
+const navOpenedClass = 'left-0';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-arrow-left-s-line';
+const arrowRightClass = 'ri-arrow-right-s-line';
+
+closeBtn.addEventListener('click', () => {
+  console.log('click!!');
+});
